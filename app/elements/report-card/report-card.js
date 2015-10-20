61141bcda67b478d8e4369a29feabd12fe55fba6
Justin Liu <justin.xin.liu@gmail.com>
2015-10-20 22:17:54 +1100

    Some experimental design with Polymer
    Based on Polymer Starter Kit, written in ES6 with Visual Studio Code.

---
+++ app/elements/report-card/report-card.js
@@ -0,0 +1,16 @@
+class ReportCard {
+	beforeRegister() {
+		this.is = 'report-card';
+		this.properties = {
+			
+		};
+	}
+
+	created() { }
+	ready() { }
+	attached() { }
+	detached() { }
+	attributeChanged() { }
+}
+
+Polymer(ReportCard);
