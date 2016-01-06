ad7729f2b285e07250de899241bfe30cd4ec09d7
Justin Liu <justin.xin.liu@gmail.com>
2016-01-07 09:10:57 +1100

    Fixed load on Firefox.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,7 @@
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  let app = Polymer.dom(document).querySelector('#app');
+  let app = document.querySelector('#app');
    
   // Sets app default base URL
   app.baseUrl = '/';
